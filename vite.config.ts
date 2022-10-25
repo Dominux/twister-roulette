23d3be9cae0f7eed17395cbb7fd8a0d39364199d
Dominux <kir.gimranov@gmail.com>
2022-10-25 13:18:39 +0300

    Added PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true },
+    }),
+  ],
   server: {
     port: 3000,
   },
   build: {
     target: 'esnext',
   },
-});
+})
